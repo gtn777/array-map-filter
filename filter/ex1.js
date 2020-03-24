ec8d3a491e32f9cb2441fb22a44251da73579d6a
gtn777 <gtn777@orange.fr>
2020-03-24 02:42:33 +0100

    filter.1 done

--- filter/ex1.js
+++ filter/ex1.js
@@ -4,17 +4,18 @@
 * It receives an array of numbers as an argument
 * Inside the function, use filter on the array of numbers to keep ONLY positive numbers (incl. zero)
 * Return the result of filter
-
-Sample numbers array, i.e. INPUT:
-  [7, -4, 2, 0, -10, 3, 11]
-
-Expected OUTPUT for this sample:
-  [7, 2, 0, 3, 11]
-
 */
+const sample =
+  [7, -4, 2, 0, -10, 3, 11];
+
+/*Expected OUTPUT for this sample:
+  [7, 2, 0, 3, 11]*/
+
 
 function getPositiveNumbers(numbers) {
+  return numbers.filter(num => num >= 0);
 }
+console.log(getPositiveNumbers(sample));
 
 // If you need, you can always visualize the result with a console.log.
 // In that case, uncomment the line below (you can also add your own
